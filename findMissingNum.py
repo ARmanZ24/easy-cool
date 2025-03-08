debd5e229a754fbb60f4516595569873979a37d8
ARmanZ24 <116273804+ARmanZ24@users.noreply.github.com>
2025-03-08 16:19:34 -0500

    Add files via upload

---
+++ findMissingNum.py
@@ -0,0 +1,16 @@
+
+usrList = list(map(int,input("Enter your list of positive non-zero numbers: ").split()))
+usrList.sort() #sorting the user given list using built-in fn
+missingNum=1 #initialize it as int
+
+if all((num>0 for num in usrList)): #Stays true only if all values in the list are > 0
+    for i in range(0,len(usrList)-1):
+        if(usrList[i+1]-usrList[i] == 1): #condition check to find the if the next number is missing in the sorted order
+            continue
+        else:
+            missingNum = usrList[i]+1
+    print("The only missing number in the given list is: ", missingNum)
+
+else:
+    print("Invalid value found in the list")
+
